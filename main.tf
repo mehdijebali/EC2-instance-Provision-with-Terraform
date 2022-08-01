06d1bd66acffd2544a62d64a8e6ea39e2236d385
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-08-01 14:51:03 +0100

    Add tf files with .gitignore

---
+++ main.tf
@@ -0,0 +1,15 @@
+## Provider's Example
+
+# Configure the AWS Provider
+# provider "aws" {
+    # version = "3.53.0"
+    # access_key = "AKIATJGNGZ4T6AEKBZMW"
+    # secret_key = "QQJkwK0YgiAlkvlCl6AYE9IMIctgwBHTPt4q1LFy"
+    # region     = "us-east-2"
+# }
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    version = "2.72.0"
+    features {}
+}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    version = "3.53.0"
+    access_key = "AKIATJGNGZ4T6AEKBZMW"
+    secret_key = "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"
+    region     = "us-east-2"
+}
