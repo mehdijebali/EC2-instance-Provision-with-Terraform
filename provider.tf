e6c3828b28a69a04b8dcf3d5f1e098de6c8c98bd
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-08-01 15:37:12 +0100

    Change aws provider version

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    version = "3.53.0"
+    version = "4.24.0"
     access_key = "AKIATJGNGZ4T6AEKBZMW"
     secret_key = "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"
     region     = "us-east-1"
