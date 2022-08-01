aeb74b108a80495ff9926b1195fa2849e9a01337
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-08-01 15:35:11 +0100

    Change aws region

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
     version = "3.53.0"
     access_key = "AKIATJGNGZ4T6AEKBZMW"
     secret_key = "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"
-    region     = "us-east-2"
+    region     = "us-east-1"
 }
